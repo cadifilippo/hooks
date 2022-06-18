23d42ea7d0d8539481d7ebf4c7200f6f6c805404
Carlos <cadifilippo@gmail.com>
2022-06-18 20:38:17 -0300

    add useAsync and useTheme

---
+++ src/hooks/useAsync.ts
@@ -0,0 +1,36 @@
+import { useState, useEffect, useCallback } from 'react';
+
+const useAsync = <T, E = string>(
+  asyncFunction: () => Promise<T>,
+  immediate = true
+) => {
+  const [status, setStatus] = useState<
+    'idle' | 'pending' | 'success' | 'error'
+  >('idle');
+  const [value, setValue] = useState<T | null>(null);
+  const [error, setError] = useState<E | null>(null);
+
+  const execute = useCallback(() => {
+    setStatus('pending');
+    setValue(null);
+    setError(null);
+    return asyncFunction()
+      .then((response: any) => {
+        setValue(response);
+        setStatus('success');
+      })
+      .catch((error: any) => {
+        setError(error);
+        setStatus('error');
+      });
+  }, [asyncFunction]);
+
+  useEffect(() => {
+    if (immediate) {
+      execute();
+    }
+  }, [execute, immediate]);
+  return { execute, status, value, error };
+};
+
+export default useAsync;
---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,19 @@
+import { useLayoutEffect } from 'react';
+
+interface Theme {
+  [name: string]: string;
+}
+
+const useTheme = (theme: Theme): void => {
+  useLayoutEffect(
+    (): void => {
+      // Iterate through each value in theme object
+      for (const key in theme) {
+        // Update css variables in document's root element
+        document.documentElement.style.setProperty(`--${key}`, theme[key]);
+      }
+    },
+    [theme] // Only call again if theme object reference changes
+  );
+};
+export default useTheme;
